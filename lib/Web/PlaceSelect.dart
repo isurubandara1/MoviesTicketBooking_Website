306471a8f8b67887eca93cefebd73b852bc0dced
Isuru <isurumbandara@gmail.com>
2024-01-24 00:18:04 +0530

    PlaceSelect.dart file update

--- lib/Web/PlaceSelect.dart
+++ lib/Web/PlaceSelect.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Home.dart';
 
 class PlaceSelect extends StatefulWidget {
   final String selectedTime;
@@ -13,6 +14,21 @@ class PlaceSelect extends StatefulWidget {
 class _PlaceSelectState extends State<PlaceSelect> {
   int? selectedFullTicket;
   int? selectedHalfTicket;
+  int price = 0;
+  Set<String> selectedButtonLabels = Set();
+
+// Calculate the price based on the selected tickets
+  void viewPrice() {
+    setState(() {
+      price = (selectedFullTicket ?? 0) * 300 + (selectedHalfTicket ?? 0) * 150;
+    });
+  }
+
+//Next buttons logic
+  bool _handleButtonPress() {
+    int totalTickets = (selectedFullTicket ?? 0) + (selectedHalfTicket ?? 0);
+    return selectedButtonLabels.length == totalTickets;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +40,7 @@ class _PlaceSelectState extends State<PlaceSelect> {
             children: [
               Container(
                 color: Colors.black87,
-                height: MediaQuery.of(context).size.height * 0.5,
+                height: MediaQuery.of(context).size.height * 0.6,
                 child: Padding(
                   padding: const EdgeInsets.only(top: 20),
                   child: Column(
@@ -40,6 +56,7 @@ class _PlaceSelectState extends State<PlaceSelect> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
+                          // Full ticket selection
                           Expanded(
                             child: Padding(
                               padding: const EdgeInsets.all(8.0),
@@ -62,6 +79,7 @@ class _PlaceSelectState extends State<PlaceSelect> {
                                         setState(() {
                                           selectedFullTicket = newValue;
                                         });
+                                        viewPrice(); // Update the price when tickets are selected
                                       },
                                       items:
                                           List<DropdownMenuItem<int>>.generate(
@@ -78,6 +96,8 @@ class _PlaceSelectState extends State<PlaceSelect> {
                             ),
                           ),
                           SizedBox(width: 30),
+
+                          // Half ticket selection
                           Expanded(
                             child: Padding(
                               padding: const EdgeInsets.all(8.0),
@@ -100,6 +120,7 @@ class _PlaceSelectState extends State<PlaceSelect> {
                                         setState(() {
                                           selectedHalfTicket = newValue;
                                         });
+                                        viewPrice(); // Update the price when tickets are selected
                                       },
                                       items:
                                           List<DropdownMenuItem<int>>.generate(
@@ -120,6 +141,8 @@ class _PlaceSelectState extends State<PlaceSelect> {
                       SizedBox(
                         height: 20,
                       ),
+
+                      // Tickets count.........
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -141,6 +164,8 @@ class _PlaceSelectState extends State<PlaceSelect> {
                       SizedBox(
                         height: 20,
                       ),
+
+                      // Time.............
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -159,12 +184,14 @@ class _PlaceSelectState extends State<PlaceSelect> {
                       SizedBox(
                         height: 10,
                       ),
+
+                      // Price............
                       Padding(
                         padding: EdgeInsets.only(
                           left: MediaQuery.of(context).size.width * 0.7,
                         ),
                         child: Text(
-                          "Price = 0",
+                          "Rs.$price/=",
                           style: TextStyle(
                               color: Colors.white,
                               fontSize: 25,
@@ -178,14 +205,16 @@ class _PlaceSelectState extends State<PlaceSelect> {
               SizedBox(
                 height: 50,
               ),
+
+              // Sheet..............
               Column(
                 children: [
                   Text(
-                    "ODC",
+                    "Sheet",
                     style: TextStyle(fontSize: 20),
                   ),
                   Text(
-                    "( F. Rs.650.00 / H. Rs.410.00 )",
+                    "( F. Rs.300.00 / H. Rs.150.00 )",
                     style: TextStyle(fontSize: 12, color: Colors.grey),
                   ),
                 ],
@@ -205,126 +234,92 @@ class _PlaceSelectState extends State<PlaceSelect> {
                 child: ODCGrid(
                   selectedFullTicket: selectedFullTicket,
                   selectedHalfTicket: selectedHalfTicket,
+                  selectedButtonLabels: selectedButtonLabels,
                 ),
               ),
               SizedBox(
-                height: 50,
+                height: 10,
               ),
-              Column(
-                children: [
-                  Text(
-                    "BALCONY",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text(
-                    "( F. Rs.760.00 / H. Rs.530.00 )",
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                color: Colors.grey,
-                height: 5,
-              ),
-              SizedBox(
-                height: 30,
-              ),
+
+              //Next button.........
               Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: BalGrid(
-                  selectedFullTicket: selectedFullTicket,
-                  selectedHalfTicket: selectedHalfTicket,
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        bool allButtonsSelected = _handleButtonPress();
+                        int totalTickets = (selectedFullTicket ?? 0) +
+                            (selectedHalfTicket ?? 0);
+
+                        if (allButtonsSelected && totalTickets > 0) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Home()),
+                          );
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text("Select Tickets"),
+                                content: Text(
+                                    "Please select at least one ticket or choose your preferred seats according to ticket count."),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text("OK"),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
+                      },
+                      child: Text(
+                        "Next",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red, // Change the button color
+                        minimumSize:
+                            Size(200, 50), // Set the minimum size of the button
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Column(
-                children: [
-                  Text(
-                    "BOX",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text(
-                    "( F. Rs.1760.00 )",
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                color: Colors.grey,
-                height: 5,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: BoxGrid(
-                  selectedFullTicket: selectedFullTicket,
-                  selectedHalfTicket: selectedHalfTicket,
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
+              )
             ],
           ),
         ),
       ),
     );
   }
-
-  void _handleButtonPress() {
-    int totalTickets = (selectedFullTicket ?? 0) + (selectedHalfTicket ?? 0);
-
-    if (totalTickets > 0) {
-      // Navigate to the next screen or perform your action here
-      print("Button pressed with ticket count: $totalTickets");
-    } else {
-      // Show a message indicating that at least one ticket should be selected
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Select Tickets"),
-            content: Text("Please select at least one ticket."),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text("OK"),
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
 }
 
 class ODCGrid extends StatefulWidget {
   final int? selectedFullTicket;
   final int? selectedHalfTicket;
+  final Set<String> selectedButtonLabels;
 
-  ODCGrid({this.selectedFullTicket, this.selectedHalfTicket});
+  ODCGrid(
+      {this.selectedFullTicket,
+      this.selectedHalfTicket,
+      required this.selectedButtonLabels});
 
   @override
   _ODCGridState createState() => _ODCGridState();
 }
 
 class _ODCGridState extends State<ODCGrid> {
-  Set<String> selectedButtonLabels = Set();
-
   @override
   Widget build(BuildContext context) {
+    int totalTickets =
+        (widget.selectedFullTicket ?? 0) + (widget.selectedHalfTicket ?? 0);
+
     return GridView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
@@ -333,14 +328,14 @@ class _ODCGridState extends State<ODCGrid> {
         crossAxisSpacing: 4,
         mainAxisSpacing: 4,
       ),
-      itemCount: 80,
+      itemCount: 250,
       itemBuilder: (context, index) {
         String buttonLabel = 'A${index + 1}';
-        bool isSelected = selectedButtonLabels.contains(buttonLabel);
+        bool isSelected = widget.selectedButtonLabels.contains(buttonLabel);
 
         return GestureDetector(
           onTap: () {
-            _handleGridItemPress(buttonLabel);
+            _handleGridItemPress(buttonLabel, totalTickets);
           },
           child: Container(
             decoration: BoxDecoration(
@@ -364,218 +359,19 @@ class _ODCGridState extends State<ODCGrid> {
     );
   }
 
-  void _handleGridItemPress(String buttonLabel) {
-    int totalTickets =
-        (widget.selectedFullTicket ?? 0) + (widget.selectedHalfTicket ?? 0);
-
-    if (totalTickets > selectedButtonLabels.length) {
+  void _handleGridItemPress(String buttonLabel, int totalTickets) {
+    if (widget.selectedButtonLabels.length < totalTickets) {
       setState(() {
-        if (selectedButtonLabels.contains(buttonLabel)) {
-          selectedButtonLabels.remove(buttonLabel);
+        if (widget.selectedButtonLabels.contains(buttonLabel)) {
+          widget.selectedButtonLabels.remove(buttonLabel);
         } else {
-          selectedButtonLabels.add(buttonLabel);
+          widget.selectedButtonLabels.add(buttonLabel);
         }
       });
       print('Button $buttonLabel pressed');
-    } else if (selectedButtonLabels.contains(buttonLabel)) {
-      // Toggle the selection if the button is already selected
+    } else if (widget.selectedButtonLabels.contains(buttonLabel)) {
       setState(() {
-        selectedButtonLabels.remove(buttonLabel);
-      });
-      print('Button $buttonLabel unselected');
-    } else {
-      // Show a message indicating that the user can't select more buttons
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Select Tickets"),
-            content: Text(
-                "You can't select more buttons than the total number of tickets."),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text("OK"),
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-}
-
-class BalGrid extends StatefulWidget {
-  final int? selectedFullTicket;
-  final int? selectedHalfTicket;
-
-  BalGrid({this.selectedFullTicket, this.selectedHalfTicket});
-
-  @override
-  _BalGridState createState() => _BalGridState();
-}
-
-class _BalGridState extends State<BalGrid> {
-  Set<String> selectedButtonLabels = Set();
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 30,
-        crossAxisSpacing: 4,
-        mainAxisSpacing: 4,
-      ),
-      itemCount: 50,
-      itemBuilder: (context, index) {
-        String buttonLabel = 'B${index + 1}';
-        bool isSelected = selectedButtonLabels.contains(buttonLabel);
-
-        return GestureDetector(
-          onTap: () {
-            _handleGridItemPress(buttonLabel);
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: isSelected ? Colors.green : Colors.grey,
-              border: Border.all(
-                color: Colors.grey,
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(4),
-            ),
-            alignment: Alignment.center,
-            child: Text(
-              buttonLabel,
-              style: TextStyle(fontSize: 10, color: Colors.black),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  void _handleGridItemPress(String buttonLabel) {
-    int totalTickets =
-        (widget.selectedFullTicket ?? 0) + (widget.selectedHalfTicket ?? 0);
-
-    if (totalTickets > selectedButtonLabels.length) {
-      setState(() {
-        if (selectedButtonLabels.contains(buttonLabel)) {
-          selectedButtonLabels.remove(buttonLabel);
-        } else {
-          selectedButtonLabels.add(buttonLabel);
-        }
-      });
-      print('Button $buttonLabel pressed');
-    } else if (selectedButtonLabels.contains(buttonLabel)) {
-      // Toggle the selection if the button is already selected
-      setState(() {
-        selectedButtonLabels.remove(buttonLabel);
-      });
-      print('Button $buttonLabel unselected');
-    } else {
-      // Show a message indicating that the user can't select more buttons
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Select Tickets"),
-            content: Text(
-                "You can't select more buttons than the total number of tickets."),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text("OK"),
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-}
-
-class BoxGrid extends StatefulWidget {
-  final int? selectedFullTicket;
-  final int? selectedHalfTicket;
-
-  BoxGrid({this.selectedFullTicket, this.selectedHalfTicket});
-
-  @override
-  _BoxGridState createState() => _BoxGridState();
-}
-
-class _BoxGridState extends State<BoxGrid> {
-  Set<String> selectedButtonLabels = Set();
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 30,
-        crossAxisSpacing: 4,
-        mainAxisSpacing: 4,
-      ),
-      itemCount: 20,
-      itemBuilder: (context, index) {
-        String buttonLabel = 'C${index + 1}';
-        bool isSelected = selectedButtonLabels.contains(buttonLabel);
-
-        return GestureDetector(
-          onTap: () {
-            _handleGridItemPress(buttonLabel);
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: isSelected ? Colors.green : Colors.grey,
-              border: Border.all(
-                color: Colors.grey,
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(4),
-            ),
-            alignment: Alignment.center,
-            child: Text(
-              buttonLabel,
-              style: TextStyle(fontSize: 10, color: Colors.black),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-// Common method for all sections
-  void _handleGridItemPress(String buttonLabel) {
-    int totalTickets =
-        (widget.selectedFullTicket ?? 0) + (widget.selectedHalfTicket ?? 0);
-
-    if (totalTickets > selectedButtonLabels.length) {
-      setState(() {
-        if (selectedButtonLabels.contains(buttonLabel)) {
-          selectedButtonLabels.remove(buttonLabel);
-        } else {
-          selectedButtonLabels.add(buttonLabel);
-        }
-      });
-      print('Button $buttonLabel pressed');
-    } else if (selectedButtonLabels.contains(buttonLabel)) {
-      // Toggle the selection if the button is already selected
-      setState(() {
-        selectedButtonLabels.remove(buttonLabel);
+        widget.selectedButtonLabels.remove(buttonLabel);
       });
       print('Button $buttonLabel unselected');
     } else {
--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home());
+    return MaterialApp(
+      home: Home(),
+    );
   }
 }
